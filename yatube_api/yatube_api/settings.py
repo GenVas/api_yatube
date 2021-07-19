c2550805291dc92be9543b29ade5ed6ab204670d
GenVas <vasilievga@yandex.ru>
2021-07-19 19:37:26 +0300

    let it work2

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    # 'djoser',
     'rest_framework.authtoken',
     'posts',
     'api',
